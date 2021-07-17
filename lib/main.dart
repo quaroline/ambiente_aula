33ae7b4d12ebaef5acc9c805810902105dde7a18
Carolynne Padilha da Silva <carodilha@gmail.com>
2021-07-16 21:39:10 -0300

    Drawer Header done.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,17 @@ void main() {
   runApp(MyApp());
 }
 
+const String imageUrl = "https://i.imgur.com/{0}.png";
+const String studentName = "Carolynne";
+
+Map imagesUris = {};
+
+enum ImageType {
+  user
+}
+
+String getImageUrl(ImageType type) => imageUrl.replaceAll("{0}", imagesUris[type.toString()]);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -27,38 +38,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  void _initializeImageMap() {
+    imagesUris = new Map();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    imagesUris[ImageType.user.toString()] = 'w65MVVH';
+  }
+
+  Widget _createDrawerHeader() {
+    return DrawerHeader(
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(32, 32, 32, 1),
+        border: Border(bottom: BorderSide(color: Colors.white38, width: 2))
+      ),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 52,
+              height: 52,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(getImageUrl(ImageType.user))
+                  )
+              )
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5),
+              child: Text(
+                studentName.toLowerCase(), 
+                style: TextStyle(color: Colors.white, fontSize: 16)
+              )
+            ),
+            Container(
+              child: Text(
+                "ALUNO", 
+                style: TextStyle(color: Colors.white38, fontSize: 14, fontWeight: FontWeight.w100)
+              )
+            )
+          ],
+        )
+      )
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    _initializeImageMap();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(title: Text("Me passa, pfv!")),
+      body: Center(child: Text('My Page!')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            _createDrawerHeader(),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
